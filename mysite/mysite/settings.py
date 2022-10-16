8d93ff5eaa7df4fe58ee88be7c49177c4b47ff75
Simone B <snb331@nyu.edu>
2022-10-16 18:26:23 -0400

    matched inner mysite to eb

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-rp6dbm2b.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
