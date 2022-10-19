dfe333d308ee5895b2ffa2466d0fa8613f4be67c
Simone B <snb331@nyu.edu>
2022-10-18 21:03:55 -0400

    updated eb stuff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-rp6dbm2b.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.eba-rp6dbm2b.us-west-2.elasticbeanstalk.com','127.0.0.1' ]
 
 
 # Application definition
