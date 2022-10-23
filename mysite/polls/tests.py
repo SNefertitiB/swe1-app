1385d1f86ce2b116b74f1866bd0b9d743b6b8455
Simone B <snb331@nyu.edu>
2022-10-23 19:20:56 -0400

    fixed black issue

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,7 +6,6 @@ client = Client()
 
 # Create your tests here.
 class FakeTest(TestCase):
-
     def test_dummy_test(self):
         response = self.client.get("/polls/")
         self.assertEqual(response.status_code, 200)
