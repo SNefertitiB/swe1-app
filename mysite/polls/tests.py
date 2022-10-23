926301349430512ad15e9322c25e3aad9f92c55e
Simone B <snb331@nyu.edu>
2022-10-23 16:52:33 -0400

    black and flake8

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
+from django.test import Client
+
+client = Client()
+
 
 # Create your tests here.
 class FakeTest(TestCase):
-    def test_dummy_test(self):
-        assertEqual(5,5)
 
+    def test_dummy_test(self):
+        response = self.client.get("/polls/")
+        self.assertEqual(response.status_code, 200)
